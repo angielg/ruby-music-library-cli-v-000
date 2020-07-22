b2d17b24c752a3ff97394e9e56a6ac46b572689d
Angie Goza <angielg9888@gmail.com>
2020-07-22 22:00:56 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -23,7 +23,7 @@ class MusicLibraryController
       elsif input == "list artists"
         list_artists
       elsif input == "list genres"
-        list genres
+        list_genres
       elsif input == "list artist"
         list_songs_by_artist
       elsif input == "list genre"
